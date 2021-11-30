718e647bfbfc9230136c06493920e70add01208c
StaticBeagle <m.lopez27@gmail.com>
2021-11-30 11:40:07 -0600

    fixed queue. Refactored all print functions to functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,111 @@
 #include "linkedlist.h"
 #include "list.h"
 #include "stack.h"
+#include "queue.h"
 #include "dictionary.h"
 #include "ordered_dictionary.h"
 
-int main()
+void test_linked_list()
 {
     LinkedList<int> linkedList;
-    std::cout << "Initial list" << std::endl;
+    std::cout << "Initial linked list" << std::endl;
     linkedList.push_back(4);
     linkedList.push_back(3);
     linkedList.print();
 
-    std::cout << "Inserted 5 to pos = 1" << std::endl;
+    std::cout << "Inserted 5 to pos = 1 to the linked list" << std::endl;
     linkedList.insert(1, 5);
     linkedList.print();
 
-    std::cout << "Inserted 6 to pos = 0" << std::endl;
+    std::cout << "Inserted 6 to pos = 0 to the linked list" << std::endl;
     linkedList.insert(0, 6);
     linkedList.print();
 
-    std::cout << "pushed 8 to front of list" << std::endl;
+    std::cout << "pushed 8 to front of linked list" << std::endl;
     linkedList.push_front(8);
     linkedList.print();
 
-    std::cout << "popped back of the list" << std::endl;
+    std::cout << "pushed 10 to back of linked list" << std::endl;
+    linkedList.push_back(10);
+    linkedList.print();
+
+    std::cout << "popped back of the linked list" << std::endl;
     linkedList.pop_back();
     linkedList.print();
 
-    std::cout << "popped front of the list" << std::endl;
+    std::cout << "popped front of the linked list" << std::endl;
     linkedList.pop_front();
     linkedList.print();
 
-    std::cout << "Cleared the linkedList" << std::endl;
+    std::cout << "Cleared the linked list" << std::endl;
     linkedList.clear();
+    std::cout << "Current linked list" << std::endl;
     linkedList.print();
+    std::cout << std::endl;
+}
 
+void test_stack()
+{
     Stack<int> stack;
-    std::cout << "Initial stack" << std::endl;
+    std::cout << "Pushed 4 into the stack" << std::endl;
     stack.push(4);
+    std::cout << "Pushed 3 into the stack" << std::endl;
     stack.push(3);
+    std::cout << "Initial stack" << std::endl;
     stack.print();
+    std::cout << "Popped the top of the stack" << std::endl;
+    stack.pop();
+    std::cout << "Current stack" << std::endl;
+    stack.print();
+    std::cout << "Popped the top of the stack" << std::endl;
+    stack.pop();
+    std::cout << "Current stack" << std::endl;
+    stack.print();
+    std::cout << std::endl;
 
+    std::cout << "Pushed 4 into the stack" << std::endl;
+    stack.push(4);
+    std::cout << "Pushed 3 into the stack" << std::endl;
+    stack.push(3);
+    std::cout << "Cleared the stack" << std::endl;
+    stack.clear();
+    std::cout << "Current stack" << std::endl;
+    stack.print();
+    std::cout << std::endl;
+}
+
+void test_queue()
+{
+    Queue<int> queue;
+    std::cout << "Pushed 4 into the queue" << std::endl;
+    queue.push(4);
+    std::cout << "Pushed 3 into the queue" << std::endl;
+    queue.push(3);
+    std::cout << "Initial queue" << std::endl;
+    queue.print();
+    std::cout << "Popped the top of the queue" << std::endl;
+    queue.pop();
+    std::cout << "Current queue" << std::endl;
+    queue.print();
+    std::cout << "Popped the top of the queue" << std::endl;
+    queue.pop();
+    std::cout << "Current queue" << std::endl;
+    queue.print();
+    std::cout << std::endl;
+
+    std::cout << "Pushed 4 into the queue" << std::endl;
+    queue.push(4);
+    std::cout << "Pushed 3 into the queue" << std::endl;
+    queue.push(3);
+    std::cout << "Cleared the queue" << std::endl;
+    queue.clear();
+    std::cout << "Current queue" << std::endl;
+    queue.print();
+    std::cout << std::endl;
+}
+
+void test_list()
+{
     List<int> list;
     std::cout << "Initial list" << std::endl;
     for (int i = 0; i < 15; ++i)
@@ -53,7 +117,11 @@ int main()
     {
         std::cout << list[i] << std::endl;
     }
+    std::cout << std::endl;
+}
 
+void test_dictionary()
+{
     // Using double as key to force collisions
     std::cout << "Dictionary " << std::endl;
     Dictionary<double, int> dict;
@@ -71,7 +139,11 @@ int main()
     dict.add(12.7, 10);
     dict.add(13.8, 12);
     dict.print();
+    std::cout << std::endl;
+}
 
+void test_ordered_dictionary()
+{
     // Using double as key to force collisions
     std::cout << "Ordered Dictionary " << std::endl;
     OrderedDictionary<double, int> ordDict;
@@ -89,4 +161,15 @@ int main()
     ordDict.add(12.7, 10);
     ordDict.add(13.8, 12);
     ordDict.print();
+    std::cout << std::endl;
+}
+
+int main()
+{
+    test_linked_list();
+    test_stack();
+    test_queue();
+    test_list();
+    test_dictionary();
+    test_ordered_dictionary();
 }
--- src/queue.h
+++ src/queue.h
@@ -15,23 +15,25 @@ public:
     Queue() : 
         m_count { 0 }
     {
-        m_base = new QueueNode<T>();
-        m_base->next = nullptr;
+        m_top = new QueueNode<T>();
     }
 
     ~Queue()
     {
         this->clear();
-        m_base->next = nullptr;
-        delete m_base;
+        m_top->next = nullptr;
+        delete m_top;
     }
 
     void push(const T& obj)
     {
-        QueueNode<T>* node = m_base;
-        m_base = new QueueNode<T>();
-        m_base->data = obj;
-        m_base->next = node;
+        QueueNode<T>* node = m_top;
+        while(node->next != nullptr)
+        {
+            node = node->next;
+        }
+        node->next = new QueueNode<T>();
+        node->next->data = obj;
         ++m_count;
     }
 
@@ -40,18 +42,18 @@ public:
         if(this->is_empty()) {
             return;
         }
-        QueueNode<T>* node = m_base;
-        m_base = m_base->next;
+        QueueNode<T>* node = m_top;
+        m_top = m_top->next;
         delete node;
         --m_count;
     }
 
     void clear()
     {
-        while(m_base->next != nullptr)
+        while(m_top->next != nullptr)
         {
-            QueueNode<T>* tmp = m_base;
-            m_base = m_base->next;
+            QueueNode<T>* tmp = m_top;
+            m_top = m_top->next;
             delete tmp;
         }
         m_count = 0;
@@ -69,14 +71,14 @@ public:
 
     void print() 
     {
-        QueueNode<T>* node = m_base;
-        while(node->next != nullptr)
+        QueueNode<T>* node = m_top->next;
+        while(node != nullptr)
         {
             std::cout << node->data << std::endl;
             node = node->next;
         }
     }
 private:
-    QueueNode<T>* m_base;
+    QueueNode<T>* m_top;
     size_t m_count;
 };
